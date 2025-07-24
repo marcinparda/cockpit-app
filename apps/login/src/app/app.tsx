543960831059a3eaffc32961b8e7a9485e91d6d6
MarcinParda <marcin98parda@gmail.com>
2025-07-25 00:59:51 +0200

    feat: create login app with tailwind v4 and shadcn setup

---
+++ apps/login/src/app/app.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@cockpit-app/shared/react-ui';
+
+export function App() {
+  return (
+    <div>
+      <h1>Welcome to the Login App</h1>
+      <Button variant="default">Login</Button>
+      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+    </div>
+  );
+}
+
+export default App;
