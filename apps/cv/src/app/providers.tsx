90ef638e82ef626ee1d49602264c3973054626bf
MarcinParda <marcin98parda@gmail.com>
2025-08-04 22:31:06 +0200

    refactor: remove unused ReactQueryDevtools import from Providers component

--- apps/cv/src/app/providers.tsx
+++ apps/cv/src/app/providers.tsx
@@ -1,6 +1,5 @@
 import { tanstackQueryClient } from '@cockpit-app/shared-react-data-access';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
