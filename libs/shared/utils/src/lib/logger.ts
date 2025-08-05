a28f7f59695c5921144ec18d03c4d96e97e75391
MarcinParda <marcin98parda@gmail.com>
2025-08-05 23:40:43 +0200

    feat: add mode constants and update logger to use environment mode

--- libs/shared/utils/src/lib/logger.ts
+++ libs/shared/utils/src/lib/logger.ts
@@ -1,16 +1,18 @@
+import { environments } from "./environments/environments";
+
 export const logger = {
   debug: (message: string, ...args: unknown[]) => {
-    if (['development'].includes(import.meta.env['MODE'])) {
+    if (['development'].includes(environments.mode)) {
       console.log(`[DEBUG] ${message}`, ...args);
     }
   },
   warn: (message: string, ...args: unknown[]) => {
-    if (['development'].includes(import.meta.env['MODE'])) {
+    if (['development'].includes(environments.mode)) {
       console.warn(`[WARNING] ${message}`, ...args);
     }
   },
   error: (message: string, ...args: unknown[]) => {
-    if (['development'].includes(import.meta.env['MODE'])) {
+    if (['development'].includes(environments.mode)) {
       console.error(`[ERROR] ${message}`, ...args);
     }
   },
