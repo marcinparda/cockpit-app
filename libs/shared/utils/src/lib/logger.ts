3e70aa18fdb5b1fdcf19c7f9508ddcfe874ba525
MarcinParda <marcin98parda@gmail.com>
2025-08-05 23:16:35 +0200

    refactor: update auth interceptor and logger for improved type safety and structure

--- libs/shared/utils/src/lib/logger.ts
+++ libs/shared/utils/src/lib/logger.ts
@@ -1,15 +1,15 @@
 export const logger = {
-  debug: (message, ...args) => {
+  debug: (message: string, ...args: unknown[]) => {
     if (['development'].includes(import.meta.env['MODE'])) {
       console.log(`[DEBUG] ${message}`, ...args);
     }
   },
-  warn: (message, ...args) => {
+  warn: (message: string, ...args: unknown[]) => {
     if (['development'].includes(import.meta.env['MODE'])) {
       console.warn(`[WARNING] ${message}`, ...args);
     }
   },
-  error: (message, ...args) => {
+  error: (message: string, ...args: unknown[]) => {
     if (['development'].includes(import.meta.env['MODE'])) {
       console.error(`[ERROR] ${message}`, ...args);
     }
