fe3d542b1d1685bd82c4848f567b4a614413e786
MarcinParda <marcin98parda@gmail.com>
2025-08-08 13:05:00 +0200

    fix: todo items are not filtered on project

--- apps/todo/src/components/TodoList.vue
+++ apps/todo/src/components/TodoList.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
+  import { useRoute } from 'vue-router';
   import { useItems } from '../composables/useTodoItems';
   import TodoItem from './TodoItem.vue';
   import { Divider } from '@cockpit-app/shared-vue-ui';
+  import { computed, watch } from 'vue';
 
+  const route = useRoute();
   const { todoItems } = useItems();
+
+  const projectTodoItems = computed(() => {
+    const projectId = route.query['project']
+      ? Number(route.query['project'])
+      : null;
+    if (projectId) {
+      return todoItems.value.filter((item) => item.project_id === projectId);
+    }
+    return todoItems.value;
+  });
 </script>
 
 <template>
   <ul class="py-8">
-    <template v-for="(item, idx) in todoItems" :key="item.id">
+    <template v-for="(item, idx) in projectTodoItems" :key="item.id">
       <TodoItem :item="item" />
-      <Divider v-if="idx < todoItems.length - 1" class="my-2" />
+      <Divider v-if="idx < projectTodoItems.length - 1" class="my-2" />
     </template>
   </ul>
 </template>
